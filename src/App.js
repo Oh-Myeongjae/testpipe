df609a60ca19b02e5c692acebdb0d2cd1bc14cd5
Oh-Myeongjae <dhaudwo1@daum.net>
2021-08-29 04:08:35 +0900

    map수정

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload123.
+          Edit <code>src/App.js</code> and save to reload12345.
         </p>
         <a
           className="App-link"
@@ -41,5 +41,5 @@ function App() {
     ></div>
     </div>
   )}
-
+ 
 export default App;
