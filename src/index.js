cb47dc2d3c546890736e5ac9ed76c7f53813fdec
Oh-Myeongjae <dhaudwo1@daum.net>
2021-08-29 02:47:02 +0900

    map수정

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Map from './MapContainer';
 
 ReactDOM.render(
   <React.StrictMode>
+    <script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=203e7359b9fc1b02acbb85ed03e2900f"></script>
     <App />
+    <Map />
   </React.StrictMode>,
   document.getElementById('root')
 );
