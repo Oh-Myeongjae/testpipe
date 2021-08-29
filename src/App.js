f7aa0f6e96e5e9054315c371ac7cbac7022260f9
Oh-Myeongjae <dhaudwo1@daum.net>
2021-08-29 23:29:47 +0900

    mapsdzxcvxz3수정

--- src/App.js
+++ src/App.js
@@ -20,12 +20,25 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload00000000000000000000000000.1
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      
       <div
       className="map"
       style={{ width: "500px", height: "500px" }}
       ref={container}
-      ></div>
-      </header>
+    ></div>
     </div>
   )}
 export default App;
